b8f2ced135b40221bd3c4459bd6d9c913b73bb37
YANG <YANG@YANG-PC>
2016-10-12 13:41:25 +0900

    week6 lab

---
+++ problem1.cpp
@@ -0,0 +1,18 @@
+//problem 1
+/*#include <iostream>
+#include <string>
+using namespace std;
+
+template<class T_CLASS>
+void print(const T_CLASS& v)
+{
+	cout << v << endl;
+}
+int main()
+{
+	print(1);
+	print(2.345f);
+	print("Hello World");
+	return 0;
+}
+*/
---
+++ problem2.cpp
@@ -0,0 +1,21 @@
+//problem2
+/*#include <iostream>
+#include <string>
+
+using namespace std;
+
+template<class T_CLASS>
+class Vector2d
+{
+public:
+	T_CLASS x_, y_;
+};
+int main()
+{
+	Vector2d<float> my_vector, my_vector2, my_vector3;
+	my_vector.x_ = 1.0f; my_vector.y_ = 2.0f;
+	cout << my_vector.x_ << endl;
+	cout << my_vector.y_ << endl;
+
+	return 0;
+}*/
